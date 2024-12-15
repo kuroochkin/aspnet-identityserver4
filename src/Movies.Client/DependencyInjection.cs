2d4a686964f947846903c58c3eeb968967a25497
kurochkinvr <pin11@kurochkin@gmail.com>
2024-12-15 15:41:24 +0300

    #feature/moviesClient *Набросал пока что замоканный клиент

---
+++ src/Movies.Client/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Movies.Client.ApiServices;
+
+namespace Movies.Client;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddMvcLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IMovieApiService, MovieApiService>();
+
+        return services;
+    }
+}
