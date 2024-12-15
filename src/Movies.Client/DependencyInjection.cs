76c9d880affa4ad8558f5262aaf0833af1ae71f5
kurochkinvr <pin11@kurochkin@gmail.com>
2024-12-15 19:59:29 +0300

    #feature/IdentityServerUI *Получение токена в MVC-Client от IdentityServer. Операция теперь проводится через UI

--- src/Movies.Client/DependencyInjection.cs
+++ src/Movies.Client/DependencyInjection.cs
@@ -1,12 +1,37 @@
-﻿using Movies.Client.ApiServices;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Movies.Client.ApiServices;
 
 namespace Movies.Client;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddMvcLayer(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddMvcLayer(this IServiceCollection services)
     {
+        services.AddControllersWithViews();
         services.AddScoped<IMovieApiService, MovieApiService>();
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                options.Authority = "https://localhost:5005";
+
+                options.ClientId = "movies_mvc_client";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code";
+                
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+
+                options.SaveTokens = true;
+
+                options.GetClaimsFromUserInfoEndpoint = true;
+            });
 
         return services;
     }
